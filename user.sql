3439faa99cb85a58450d522db0151c6108dcdef0
mingmeng <yrx990327@163.com>
2017-02-25 10:45:24 +0800

    test

---
+++ user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `user_name`, `user_pw`, `user_information`) VALUES (6, 'iop', 'df6d2338b2b8fce1ec2f6dda0a630eb0', 'iop_inf');
+INSERT INTO `user` (`id`, `user_name`, `user_pw`, `user_information`) VALUES (7, 'qwe', '202cb962ac59075b964b07152d234b70', 'qwe_inf');
+INSERT INTO `user` (`id`, `user_name`, `user_pw`, `user_information`) VALUES (8, 'asd', '202cb962ac59075b964b07152d234b70', 'asd_inf');
